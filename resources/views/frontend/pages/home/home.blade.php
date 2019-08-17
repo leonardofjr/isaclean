5205f69f7262d964bc72622c800a22c18b169293
Leo <hello@leojr.me>
2019-08-17 14:30:13 -0400

    /

--- resources/views/frontend/pages/home/home.blade.php
+++ resources/views/frontend/pages/home/home.blade.php
@@ -4,13 +4,13 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-8">
-                    <h1 class="h1">BEST OFFICE CLEANING & JANITORIAL SERVICES IN MARKHAM AND SURROUNDING AREAS</h1>
-                    <p>Stop wasting money for poor janitorial cleaning services. BEST OFFICE CLEANING & JANITORIAL SERVICES IN MARKHAM AND SURROUNDING AREAS</h1>
+                    <h1 class="h1 py-2">BEST OFFICE CLEANING & JANITORIAL SERVICES IN MARKHAM AND SURROUNDING AREAS</h1>
+
                     <p>Stop wasting money for poor janitorial cleaning services</p>
                     <button class="btn btn-primary">Get a Free Quote</button>
                 </div>
                 <div class="col-lg-4">
-                    <img src="imgs/janitor-dust-mopping.gif" alt="The image displays janitor dust mopping and a mop bucket to his right.">
+                    <img class="img-fluid py-3" src="imgs/janitor-dust-mopping.gif" alt="The image displays janitor dust mopping and a mop bucket to his right.">
                 </div>
             </div>
          </div>
@@ -20,7 +20,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-8">
-                    <h2 class="h2">Welcome to ISA Commercial Cleaning Services</h2>
+                    <h2 class="h2 py-2">Welcome to ISA Commercial Cleaning Services</h2>
                     <h3 class="h3">CLEANING OFFICES, SCHOOLS AND MUCH MORE FOR OVER 20 YEARS</h3>
                     <p>For over 20 years we have provided professional janitorial services in Markham and surrounding areas such as Whitchurch Stouffville and Richmond Hill</h1>
                 </div>
