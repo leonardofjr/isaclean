666356050186e6d2779d0e08945fcaafd58685aa
Leo <hello@leojr.me>
2019-08-20 16:37:48 -0400

    /

--- resources/views/frontend/pages/about/about.blade.php
+++ resources/views/frontend/pages/about/about.blade.php
@@ -1,27 +1,31 @@
 @extends('frontend.layouts.app')
 @section('content')
-    <section class="">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-6  bg-secondary">
+    <section class="container-fluid">
+           <div class="">
+              <div class="row">
+                  <div class="col-lg-6  bg-secondary">
 
-                </div>
-                <div class="col-lg-6">
-                  <h1 class="h1">Why us?</h1>
-                    <p>For over 20 years we’ve provided all sorts of cleaning services such as school cleaning services, as well as office cleaning services in Markham.</p>
-                </div>
-            </div>
-            <div class="row">
+                  </div>
                   <div class="col-lg-6">
-                  <h2 class="h2">OUR CUSTOMER EXPERIENCE AND COMMUNICATION ENSURES YOU GET EVERY BANG FOR YOUR BUCK</h2>
-                  <p>We do have dedicated teams of personnel with hands on experience that are always committed to provide reliable and competitive services to all our clients. We execute all our jobs timely to a high safety and quality standard.</p>
-                  <ul>
-                    <li>Excellent communication and response rate to provide you with the best janitorial services</li>
-                    <li>Bonded & Insured for your peace of mind</li>
-                    <li>Performance Assurance Program ensures you get the best commercial cleaning service you deserve</li>
-                  </ul>
-                  <div class="col-lg-6  bg-secondary"></div>
-                </div>
+                    <h1 class="h1">Why us?</h1>
+                      <p>For over 20 years we’ve provided all sorts of cleaning services such as school cleaning services, as well as office cleaning services in Markham.</p>
+                  </div>
+              </div>
+            </div>
+            <div class="container">
+              <div class="row">
+                    <div class="col-lg-6">
+                    <h2 class="h2">OUR CUSTOMER EXPERIENCE AND COMMUNICATION ENSURES YOU GET EVERY BANG FOR YOUR BUCK</h2>
+                    <p>We do have dedicated teams of personnel with hands on experience that are always committed to provide reliable and competitive services to all our clients. We execute all our jobs timely to a high safety and quality standard.</p>
+                    <ul>
+                      <li>Excellent communication and response rate to provide you with the best janitorial services</li>
+                      <li>Bonded & Insured for your peace of mind</li>
+                      <li>Performance Assurance Program ensures you get the best commercial cleaning service you deserve</li>
+                    </ul>
+                  </div>
+                    <div class="col-lg-6 image-4">
+                  </div>
+              </div>
             </div>
          </div>
 
