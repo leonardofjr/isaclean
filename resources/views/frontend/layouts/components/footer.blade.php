f9ef10fd3881da7199d1ca2fb0edfad764c89670
Leo <hello@leojr.me>
2019-09-06 14:34:31 -0400

    /

--- resources/views/frontend/layouts/components/footer.blade.php
+++ resources/views/frontend/layouts/components/footer.blade.php
@@ -1,11 +1,12 @@
 <div class="container">
-    <div class="row">
-        <div class="col-md-3">
+    <div class="row py-5">
+        <div class="col-md-8">
             <ul class="navbar-nav">
+                    <h4>WE MAKE SURE YOU GET THE BEST CLEANING SERVICE FOR YOUR BUCK</h3>
                     @include('frontend.menus.primary')
             </ul>
         </div>
-        <div class="col-md-9">
+        <div class="col-md-4">
             <img src="/imgs/isa-van-right.png" class="img-fluid" alt="An image of a white cargo van with the isa commercial cleaning service logo">
         </div>
     </div>
--- resources/views/frontend/pages/blog-page.blade.php
+++ resources/views/frontend/pages/blog-page.blade.php
@@ -7,8 +7,16 @@
     <section class="container">
         <div class="row">
             <div class="col-lg-9">
-                <h2>Why Should I pay for Cleaning Services?</h2>
-                <p>Everybody wants a clean office. Unfortunately, we don’t always have the necessary time to clean. Hiring a professional cleaning service gives you the freedom to get on with your day while someone else gets rid of the dirt. If you’re not sure about hiring a...</p>
+                <div>
+                        <h2>Why Should I pay for Cleaning Services?</h2>
+                        <p>Everybody wants a clean office. Unfortunately, we don’t always have the necessary time to clean. Hiring a professional cleaning service gives you the freedom to get on with your day while someone else gets rid of the dirt. If you’re not sure about hiring a professional cleaning service, we have four reasons you should:<br><br></p>
+                        
+                        <ol><li><strong>We’ll get into every corner</strong><br>Professional cleaners are just that: professionals. We will give a full scrub down of every single corner of your office.  There are areas that you probably don’t want to touch anyway, like the toilet or behind the fridge. We’ll make sure they shine and we’ll do it with smiles on our faces.</li><li><strong>We’ll finish the cleaning job quick and effectively</strong><br>We also know how to clean quickly but effectively. Our techniques have been perfected over the years and when we show up at your facility, we’re ready to work. We’re neat freaks by trade so it’s important for us to leave your office free of dirt, dust, and stains when we’re done.</li><li><strong>We keep whatever schedule you want us to</strong><br>The great thing about hiring a professional cleaning service is we work when you want us to. You can schedule cleanings, weekly, bi-weekly or monthly—basically, whenever you want. We’ll also come to your office when it’s closed so as to not interfere with your employees while they’re working.</li><li><strong>We only use products that are safe</strong><br>Industrial cleaning products are sometimes a must when it comes to cleaning. However, that doesn’t mean we’ll use products that are harmful to the environment and your employees. </li></ol>
+                        
+                        
+                        
+                        <p>Having a clean facility is an important part of your everyday well-being. You don’t want to live in a pigsty or work in one, which is why you should hire a professional cleaning company. Contact ISA Commercial Cleaning Services and we’ll turn your office into a sparkling environment. Plus, you can sit back and relax while we do all the dirty work.</p>
+                </div>
                 <a href="/news"><button class="btn btn-primary">Back</button></a>
             </div>
              <div class="col-lg-3">
